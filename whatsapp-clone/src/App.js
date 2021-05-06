02485495dda97322d21606b8f4a17a40aace96ed
Yash Garg <yashgarg5101@gmail.com>
2021-05-06 11:43:32 +0530

    made create react app template

---
+++ whatsapp-clone/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>This is a whatsapp mern clone</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ whatsapp-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBlUVd3cTxVwjibrmXQoAXqh75GnY5LD7Q",
+    authDomain: "whatsapp-mern-6abcc.firebaseapp.com",
+    projectId: "whatsapp-mern-6abcc",
+    storageBucket: "whatsapp-mern-6abcc.appspot.com",
+    messagingSenderId: "918319720824",
+    appId: "1:918319720824:web:ad4e2c23a270a0cb53408d"
+  };
